b05702ecdd3e03e8f1001826b326c23b710b90c3
yichisharen <347312881@qq.com>
2021-05-25 16:58:32 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FistClass.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
